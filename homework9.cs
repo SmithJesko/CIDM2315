a0ccb26b152a0606893f0e490358ceea3296c8ab
Smith Jesko <misc@smithjesko.com>
2023-11-01 10:29:18 -0500

    upload homework9

---
+++ homework9.cs
@@ -0,0 +1,72 @@
+﻿namespace homework9;
+using System;
+
+
+//Q1
+public class Student
+{
+    private int studentID;
+    private string studentName;
+
+    public static List<Student> studentList = new List<Student>();
+
+    public Student(int id, string name)
+    {
+        this.studentID = id;
+        this.studentName = name;
+        studentList.Add(this);
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"ID: {studentID}, Name: {studentName}");
+    }
+
+    public string Name => studentName;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Q2
+        new Student(111, "Alice");
+        new Student(222, "Bob");
+        new Student(333, "Cathy");
+        new Student(444, "David");
+
+        // Q3
+        Dictionary<string, double> gradebook = new Dictionary<string, double>
+        {
+            { "Alice", 4.0 },
+            { "Bob", 3.6 },
+            { "Cathy", 2.5 },
+            { "David", 1.8 }
+        };
+
+        // Q4
+        if (!gradebook.ContainsKey("Tom"))
+        {
+            gradebook.Add("Tom", 3.3);
+        }
+
+        // Q5
+        double totalGPA = 0;
+        foreach (var gpa in gradebook.Values)
+        {
+            totalGPA += gpa;
+        }
+        double averageGPA = totalGPA / gradebook.Count;
+        Console.WriteLine($"The average GPA is: {averageGPA}");
+
+        // Q6
+        foreach (var student in Student.studentList)
+        {
+            double gpa = gradebook[student.Name];
+            if (gpa > averageGPA)
+            {
+                student.PrintInfo();
+            }
+        }
+    }
+}
